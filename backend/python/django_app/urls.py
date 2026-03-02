eada8da802c444fd68f6781d7ba18d2fa803088c
Shivansh Bhatnagar <shivansh111sid@gmail.com>
2026-03-02 20:27:53 +0530

    Added a new API call for printing a customized hello world message

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def hello_world(request):
     return HttpResponse("Hello, world! This is our interneers-lab Django server.")
 
+def hello_name(request):
+    name = request.GET.get('name', 'world')
+    return JsonResponse({'message': f'Hello, {name}!'})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world),
+    path('hello/', hello_name),
 ]
